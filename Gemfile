0695ee9a0cd84ff5ccb289cf5af9242573857f8a
Baboucarr <baboucarrd43@gmail.com>
2017-11-15 11:53:41 +0000

    Suna website first

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+ruby RUBY_VERSION
+
+# This will help ensure the proper Jekyll version is running.
+# Happy Jekylling!
+gem "jekyll", "3.6.2"
---
+++ assets/scripts/main.js
@@ -0,0 +1,36 @@
+// Mobile menu
+$(function()  {
+  $('.js-menu-toggle').on('click', function()  {
+    //$('.js-main-menu').toggleClass('is-open');
+    $('.js-main-menu__list').slideToggle();
+  });
+});
+
+// Accordion
+$(document).ready(function()  {
+ $(".js-accordion").on("click", ".js-accordion__title", function() {
+    $(this).toggleClass("is-active").next().toggle();
+ });
+});
+
+
+//fixed sidebar
+$(document).ready(function()  {
+
+  /* it seems javascript..*/
+  var topLimit = $('.js-sidebar-fixed').offset().top;
+
+  $(window).scroll(function() {
+    //console.log(topLimit <= $(window).scrollTop())
+    if (topLimit <= $(window).scrollTop()) {
+      $('.js-sidebar-fixed').addClass('sidebar-fixed')
+    } else {
+      $('.js-sidebar-fixed').removeClass('sidebar-fixed')
+    }
+  })
+
+});
+
+
+
+
